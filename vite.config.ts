a61ba075a0f388530ea627530976ae6e8a2f0da1
proaccaunte2-eng <proaccaunte2@gmail.com>
2026-01-09 20:47:22 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,14 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // Переименовываем папку ресурсов, чтобы избежать конфликта с файлом 'public' на GitHub
+  // Используем 'static' вместо 'public', так как 'public' занят файлом-ошибкой на GitHub
   publicDir: 'static',
   server: {
     port: 3000
   },
   build: {
     outDir: 'dist',
+    emptyOutDir: true,
     rollupOptions: {
       input: {
         main: './index.html',
