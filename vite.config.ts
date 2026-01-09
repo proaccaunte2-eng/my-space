0c55054c8d717f99fe11ea18ac17ee3174de1a6c
proaccaunte2-eng <proaccaunte2@gmail.com>
2026-01-09 20:27:25 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,16 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  publicDir: 'public',
   server: {
     port: 3000
   },
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
   }
 });
